e9f11ec4a0a40eff4998630e3d7930fb80734523
George-BarrBrown <gbb7899@gmail.com>
2018-12-24 19:37:54 +1300

    working on wordValidator

--- hangman.js
+++ hangman.js
@@ -7,11 +7,24 @@ function getWord() {
   myWord = wordList[Math.floor(Math.random()*wordList.length)];
   // console.log(myWord);
 }
+
+function validateLetter(target) {
+  var textInput = target.value;
+  var replacedInput = textInput.replace(/[A-Za-z]/g, "");
+  if (textInput != replacedInput) {
+    alert("You can only enter letters!");
+
+  }
+  target.value = replacedInput;
+}
+
 function revealedWord() {
   displayWord = "";
   for (var i = 0; i < myWord.length; i++) {
     if (guesses.includes(myWord[i])) {
-      
+      displayWord += word[i];
+    } else {
+
     }
   }
 }
