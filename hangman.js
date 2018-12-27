f9095aa74f77a82a4cff0ae2d991c61eaf2ce812
George-BarrBrown <gbb7899@gmail.com>
2018-12-27 14:57:33 +1300

    progress - takeUserInput, displayPartialWord, lives etc

--- hangman.js
+++ hangman.js
@@ -1,34 +1,84 @@
+document.addEventListener('DOMContentLoaded', setUpGame)
+
+var gameLost = false;
+var lives = 3;
 var wordList = ["cat", "dog", "Bike", "Milk"];
-var myWord = "";
+var myWord = "dog";
 var guesses = [];
 var displayWord = "";
+var alphaArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var guessInputElement = document.getElementById("guessInput");
+
 
 function getWord() {
   myWord = wordList[Math.floor(Math.random()*wordList.length)];
   // console.log(myWord);
 }
 
-function validateLetter(target) {
-  var textInput = target.value;
-  var replacedInput = textInput.replace(/[A-Za-z]/g, "");
-  if (textInput != replacedInput) {
-    alert("You can only enter letters!");
 
+function alreadyGuessed(letter) {
+  if (!guesses.includes(letter)) {
+    console.log("not already guessed, added to guessed letters and checking if in word");
+    guesses.push(letter);
+    // console.log(guesses);
+    inWord(letter);
+  } else {
+    console.log("already in guesses");
+    console.log(guesses);
   }
-  target.value = replacedInput;
 }
 
-function revealedWord() {
+
+function checkLetter() {
+  var letter = (document.getElementById("guessInput").value.toLowerCase());
+  // console.log(letter);
+  if (alphaArr.includes(letter.toLowerCase())) {
+    // console.log("that's a letter!");
+    alreadyGuessed(letter);
+  } else {
+    // console.log("Sorry letters only!");
+    document.getElementById("message").innerHTML = "Sorry Letters Only";
+  }
+  document.getElementById("guessInput").value = "";
+}
+
+
+function createDisplayWord() {
   displayWord = "";
+  console.log(displayWord + " Testing before");
   for (var i = 0; i < myWord.length; i++) {
     if (guesses.includes(myWord[i])) {
-      displayWord += word[i];
+      displayWord += (myWord[i] + " ");
     } else {
+      displayWord += "_ ";
+    }
+  }
 
+  console.log(displayWord + " Testing after");  
+}
+
+
+function inWord(letter) {
+  if (myWord.indexOf(letter) != -1) {
+    createDisplayWord();
+  } else {
+    lives--;
+
+    console.log("Lives: " + lives);
+    if (lives == 0) {
+      gameLost = true;
+      console.log("(x_x)")
     }
   }
 }
 
-function setUpGame() {
 
+function displayHangman() {
+  // if () {}
+}
+
+function setUpGame() {
+  // console.log("initialise game")
+  // getWord();
 }
